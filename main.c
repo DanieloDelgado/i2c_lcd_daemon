f0cd8ba58a2e3209442a24b708f49962a77f0920
Daniel O. Delgado <danielo.delgado@gmail.com>
2015-10-16 18:43:36 -0500

    Open socket

--- main.c
+++ main.c
@@ -3,8 +3,41 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <string.h>
 #include <stdbool.h>
+
+#define BAD_SOCKET(s)	((s) == -1)
+#define CLIENTS 5
+
+static int filesock(char *filename)
+{
+    struct sockaddr_un addr;
+    int sock;
+
+    if (BAD_SOCKET(sock = socket(AF_UNIX, SOCK_STREAM, 0))) {
+		return -1;
+    }
+	int a =  strlen(filename);
+    if (strlen(filename) < sizeof(addr.sun_path))
+		strcpy(addr.sun_path, filename);
+	else
+		return -1;
+    addr.sun_family = (sa_family_t)AF_UNIX;
+    
+    if (bind(sock, (struct sockaddr *)&addr, (int)sizeof(addr)) < 0) {
+			close(sock);
+	return -1;
+    }
+    if (listen(sock, CLIENTS) == -1) {
+		close(sock);
+		unlink(filename);
+		return -1;
+    }
+
+    return sock;
+}
 
 static void usage(char *argv[])
 {
@@ -15,6 +48,7 @@ static void usage(char *argv[])
 }
 
 int main(int argc, char *argv[]){
+	int socket_i2c;
 	pid_t pid = getpid();
 	bool daemonize = false;
 	int option;
@@ -34,11 +68,14 @@ int main(int argc, char *argv[]){
 		if(daemon(0,0) != 0)
 			exit(1);
 	}
-
+	char *filename = "./i2c.socket";
+	socket_i2c = filesock(filename);
 	int a=0;
 	while (a< 5){
-		sleep (1);
+		sleep (5);
 		a++;
 	}
+	close(socket_i2c);
+	unlink(filename);
 	return 0;
 }
