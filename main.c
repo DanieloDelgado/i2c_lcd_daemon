152827f7896ece904ad6097f09ea140a28976a01
Daniel O. Delgado <danielo.delgado@gmail.com>
2015-10-16 11:36:33 -0500

    Add daemonize option

--- main.c
+++ main.c
@@ -4,14 +4,41 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdbool.h>
+
+static void usage(char *argv[])
+{
+	printf("usage: %s [-d]\n\
+	Options include: \n\
+	-d		= run as daemon\n",
+	argv[0]);
+}
 
 int main(int argc, char *argv[]){
 	pid_t pid = getpid();
-	printf("%d\n",pid);
-	if(daemon(0,0) != 0)
-		exit(1);
-	while (1){
-	sleep (1);
+	bool daemonize = false;
+	int option;
+	while ((option = getopt(argc, argv, "d")) != -1){
+		switch (option){
+			case 'd':
+				daemonize = true;
+				break;
+			case 'h':
+			case '?':
+			default:
+				usage(argv);
+				exit(EXIT_SUCCESS);
+		}
+	}
+	if (daemonize){
+		if(daemon(0,0) != 0)
+			exit(1);
+	}
+
+	int a=0;
+	while (a< 5){
+		sleep (1);
+		a++;
 	}
 	return 0;
 }
